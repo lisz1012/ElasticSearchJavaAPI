4950e6ad3cdb7ea083fafd597a5edea586245d87
lisz1012 <lisz1012@163.com>
2020-07-13 23:19:38 -0700

    Add notes

--- src/test/java/com/lisz/test/Test1.java
+++ src/test/java/com/lisz/test/Test1.java
@@ -7,6 +7,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/*
+责任链 + 模版方法 设计模式，好处是再添加新的xxx_values的时候这个类几乎不用修改，
+只要多写一句calculators.add(new XXXCalculator())就好了
+ */
 public class Test1 {
 	@Test
 	public void test() {
@@ -14,6 +18,7 @@ public class Test1 {
 		calculators.add(new TotalValueCalculator());
 		calculators.add(new NormalizedValueCalculator());
 		calculators.add(new UnfilledValueCalculator());
+
 		Map<String, Map<KPI, Long>> res = new HashMap<>();
 		ValueRecord record = new ValueRecord(10L, 6L);
 		calculators.forEach(c -> {
